cbca8d3e56f513271b0d26cbfc06256b095b24f9
Zyssar <cesar.cabrera.pe@gmail.com>
2024-12-01 05:18:15 -0500

    algo pequeño update_user

--- api usuarios/update_user.py
+++ api usuarios/update_user.py
@@ -8,23 +8,27 @@ user_table = dynamodb.Table('Usuarios')
 def lambda_handler(event, context):
     try:
         # Validar token
-        token = event['headers'].get('Authorization')
-        token_data = validate_token(token)
+        tenant_id = event['headers'].get('tenant_id', '')
+        token = event['headers'].get('Authorization', '')
+        token_data = validate_token(tenant_id, token)
+
         if not token_data:
             return {
                 'statusCode': 401,
                 'body': json.dumps({'message': 'Token inválido o expirado.'})
             }
 
+        # Validar si el usuario pertenece al tenant del token
         tenant_id = token_data['tenant_id']
         user_id = event['pathParameters']['user_id']
         body = json.loads(event['body'])
 
-        # Actualizar usuario
+        # Construir expresiones de actualización
         update_expression = "SET " + ", ".join([f"#{key} = :{key}" for key in body.keys()])
         expression_attribute_names = {f"#{key}": key for key in body.keys()}
         expression_attribute_values = {f":{key}": value for key, value in body.items()}
 
+        # Actualizar usuario
         user_table.update_item(
             Key={'tenant_id': tenant_id, 'user_id': user_id},
             UpdateExpression=update_expression,
